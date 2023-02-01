d50e8dd0329d731cf4d464e87ca650fd32dc243f
Chirag Tankan <118548447+Tankanchirag@users.noreply.github.com>
2023-02-01 09:14:44 +0530

    Update and rename .gitignore to Maun.py

---
+++ Maun.py
@@ -0,0 +1,14 @@
+random. random()
+#To get any Rendon value.
+rendom.uniform(1, 10)
+#To get 1 to 10any floating random value
+rendom. rendint(1, 10) 
+#To get in tiger values
+Greeting = ['hello', 'hi', 'yo',] 
+rendom. choice(greeting) 
+#To select rendomly
+Greeting = ['hello', 'hi', 'yo',] 
+Result = rendom. choices(greeting, k = 10) 
+print(result) 
+Result = rendom. choices(greeting, weights=[18, 18,2],k = 10) 
+
